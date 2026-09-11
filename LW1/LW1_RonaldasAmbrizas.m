693ff93ec646a767a8dc0f69da2f310f711ce5ee
Ronaldas777 <ambrizasronaldas@gmail.com>
2026-09-11 15:14:23 +0300

    Update

--- LW1/LW1_RonaldasAmbrizas.m
+++ LW1/LW1_RonaldasAmbrizas.m
@@ -1,3 +1,13 @@
 % Ronaldas Ambrizas EKfu25 2026-09-11 
+%
+% Paprastas skriptas
+%
 
+x=1:32;
+y=x.^2;
+
+plot(x,y,'o-r',x,y/3,'xb')
+title('Dvi funkcijos')
+xlabel('X-ai')
+ylabel('F_1 [-o-]    |    F_2 [-x-]')
 
